1b5c49b2a2c3ef609c934b47e0be3c96b4131a0b
Frank Böker <boeker.frank@gmail.com>
2023-04-24 12:12:47 +0200

    works

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import { server } from "./server.js";
+
+const port = 8003;
+server.listen(port, () => {
+  console.log(`RandomText Server running at http://127.0.0.1:${port}`);
+});
 // import { server } from "./server.js";
 
 // server.listen(…
--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
+export const server = createServer((request, response) => {
+  const chance = new Chance();
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
+  const animal = chance.animal();
+  const word = chance.word();
+  response.statusCode = 200;
+  response.end(
+    `Hello, my name is ${name} and im am ${age} yeras old. Im am a ${profession}.
+    My best password is: "${animal}${word} ".
+    `
+  );
+});
 // export const server = …
